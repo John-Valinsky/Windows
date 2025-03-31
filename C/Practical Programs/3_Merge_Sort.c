8d2df950cf158b53ad7bbeace9c43bd683a34b27
John-Valinsky <johnvalinsky07@gmail.com>
2025-04-01 01:50:37 +0530

    Practical 3 Merge Sort

---
+++ C/Practical Programs/3_Merge_Sort.c
@@ -0,0 +1,88 @@
+// 3. Perform the Merge sort on the input {75,8,1,16,48,3,7,0) and display the output in descending order. 
+
+#include<stdio.h>
+#include<conio.h>
+
+void Merge(int A[10], int low, int mid,int high)
+{
+    int i,j,k,c[10];
+    i=low;
+    k=low;
+    j=mid+1;
+
+    while((i<=mid) && (j<=high))
+    {
+	if(A[i] <= A[j])
+	{
+	    c[k]=A[i];
+	    i++;
+	    k++;
+	}
+	else
+	{
+	    c[k] = A[j];
+	    j++;
+	    k++;
+	}
+
+    }
+
+    while(i<=mid)
+    {
+	c[k] = A[i];
+	k++;
+	i++;
+    }
+
+    while(j<=high)
+    {
+	c[k]=A[j];
+	k++;
+	j++;
+    }
+
+    for(i=low; i<=k-1; i++)
+    A[i]=c[i];
+}
+void MergeSort(int A[10], int low, int high)
+{
+    int mid;
+    if(low<high)
+    {
+        mid=(low+high)/2;
+        MergeSort(A,low,mid);
+	   MergeSort(A,mid+1,high);
+        Merge(A,low,mid,high);
+    }
+}
+
+void main()
+{
+    int i, n, A[10];
+    clrscr();
+
+    printf("\n Enter the number of elements of array: ");
+    scanf("%d",&n);
+
+    printf("\n Enter the elements of the array: ");
+    for(i=0; i<n; i++)
+	scanf("%d",&A[i]);
+
+    MergeSort(A,0,n-1);
+
+    printf("\n Sorted list of elements: ");
+    for(i=n-1; i>=0; i--)
+	printf(" %d", A[i]);
+
+    getch();
+}
+
+
+
+
+
+
+
+
+
+
