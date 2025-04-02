ad519969087bef3ec56fbd1b7d75440eb388a3be
John-Valinsky <johnvalinsky07@gmail.com>
2025-04-03 00:48:35 +0530

    Prac 8

---
+++ C/Practical Programs/8_Circular.c
@@ -0,0 +1,2 @@
+// 8. Write a program to inert the elements {5,7,0,0,3,9} into circular queue and delete 6,9&5 from it(using linked 
+// Write a Program to implement a Circular Queue Using Linked List
