aae7097e77507e0764f65dff0518565fb4820acf
John-Valinsky <johnvalinsky07@gmail.com>
2025-04-05 01:38:07 +0530

    10 and 11

---
+++ Java/11 User Define Package/Animal.java
@@ -0,0 +1,8 @@
+import forest.Tiger;
+
+public class Animal {
+    public static void main(String args[]) {
+        Tiger t1 = new Tiger();
+        t1.getDetails("Everest", 50);
+    }
+}
---
+++ Java/11 User Define Package/forest/Tiger.java
@@ -0,0 +1,8 @@
+package forest;
+
+public class Tiger {
+    public void getDetails(String nickName, int weight) {
+        System.out.println("Tiger nick name is " + nickName);
+        System.out.println("Tiger weight is " + weight);
+    }
+}
