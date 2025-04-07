1ad09bf5e5b1a3617a5e7e060a75a265b6c181f6
John-Valinsky <johnvalinsky07@gmail.com>
2025-04-07 21:00:54 +0530

    update

--- C/Practical Codes/Binary Search/Binary Search.c
+++ C/Practical Codes/Binary Search/Binary Search.c
@@ -1,52 +1,60 @@
 // 1. Given {4,7,3,2,1,7,9,0, find the location of 7 using Binary search and also display its first occurrence. 
 
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
 
-int binsearch(int A[], int n, int key)
-{
-	int first, last, mid;
-	first 	= 0;
-	last 	= n-1;
-	while (first<=last)
-	{
-		mid = (first + last)/2;
-		if(key==A[mid])
-			return mid;
-		else if (key < A[mid])
-			last = mid -1;
-		else
-			first = mid + 1;
-	}
-	return -1;
+// Function to perform Binary Search and return the first occurrence
+int binsearch_first(int A[], int n, int key) {
+    int first = 0, last = n - 1, mid;
+    int result = -1;
+
+    while (first <= last) {
+        mid = (first + last) / 2;
+
+        if (key == A[mid]) {
+            result = mid;       // Possible first occurrence
+            last = mid - 1;     // Continue searching in the left half
+        }
+        else if (key < A[mid]) {
+            last = mid - 1;
+        }
+        else {
+            first = mid + 1;
+        }
+    }
+
+    return result;
 }
 
-void main()
-{
-	int A[20], n, i, key, pos;
-	clrscr();
+int main() {
+    int A[100], n, i, key, pos;
 
-	printf("\n Enter Number of Elements: ");
-	scanf("%d", &n);
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
 
-	printf("\n Enter Elements of an Array in Ascending Order \n\n");
-	for(i = 0; i < n; i++)
-	{
-		printf(" Enter Element %d: ", i+1);
-		scanf("%d",&A[i]);
-	}
-	printf("\n Enter an Element to be Searched: ");
-	scanf("%d",&key);
+    if (n <= 0 || n > 100) {
+        printf("Invalid input. Enter between 1 and 100.\n");
+        return 1;
+    }
 
-	pos=binsearch(A,n,key);
+    printf("Enter elements in ascending order:\n");
+    for (i = 0; i < n; i++) {
+        printf("Element %d: ", i + 1);
+        scanf("%d", &A[i]);
+    }
 
-	if(pos==-1)
-		printf("\n The element %d is not found in Array \n",key);
-	else
-		printf("\n The element %d is found at Position %d \n", key, pos+1);
+    printf("Enter the element to search: ");
+    scanf("%d", &key);
 
-	getch();
+    pos = binsearch_first(A, n, key);
+
+    if (pos == -1)
+        printf("The element %d was not found in the array.\n", key);
+    else
+        printf("The element %d was found at position %d (first occurrence).\n", key, pos + 1);
+
+    return 0;
 }
+
 
 
 
