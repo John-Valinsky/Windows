515ec9e8bb61ac30a7dc5061ddaadec96bc5155e
John-Valinsky <johnvalinsky07@gmail.com>
2025-04-10 01:41:40 +0530

    update

--- C/Practical Codes/Binary Search Tree/Binary Search Tree.c
+++ C/Practical Codes/Binary Search Tree/Binary Search Tree.c
@@ -1,4 +1,4 @@
-//13. Write a program to create binary search tree with the elements {2,5,1,3,9,0,6) and perform inorder, preorder and post order traversal. 
+// Write a program to create binary search tree with the elements {2,5,1,3,9,0,6) and perform inorder, preorder and post order traversal. 
 
 #include<stdio.h>
 #include<stdlib.h>
