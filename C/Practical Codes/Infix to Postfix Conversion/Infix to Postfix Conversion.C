beb4d1247be1e0b71499793fe58eb197b1c2f6e1
John-Valinsky <johnvalinsky07@gmail.com>
2025-04-12 01:40:06 +0530

    update

--- C/Practical Codes/Infix to Postfix Conversion/Infix to Postfix Conversion.C
+++ C/Practical Codes/Infix to Postfix Conversion/Infix to Postfix Conversion.C
@@ -1,80 +1,83 @@
-// 10. Write a program to convert an infix expression x^y/(5*2)+2 to its postfix expression. Converting Infix to Postfix
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>  // For isalnum()
 
-#include<stdio.h>
-#include<string.h>
-#define MAX 20
-char s[MAX];
-int top=0;
+#define MAX 100
 
-int precedence(char elem);
-void push(char elem);
-int pop();
-void main()
-{
-	char infix[MAX], postfix[MAX],ch,elem;
-	int i=0,j=0;
+char stack[MAX];
+int top = -1;
 
-	clrscr();
-	printf("\n\t\t Program to Convert Infix to Postfix Expression");
-	printf("\n\t\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-	printf("\n Enter the infix expression: ");
-	scanf("%s", infix);
-	push('#');
-
-	for(i=0; i<strlen(infix); i++)
-	{
-		ch=infix[i];
-		if(isalnum(ch))
-			postfix[j++]=ch;
-		else if(ch=='(')
-			push(ch);
-		else if(ch==')')
-		{
-			while(s[top]!='(')
-				postfix[j++] = pop();
-			      //elem = pop();
-		}
-		else
-		{
-			while(precedence(s[top]) >= precedence(ch))
-			postfix[j++] = pop();
-			push(ch);
-		}
-	}
-
-
-	while(s[top] != '#')
-	postfix[j++]=pop();
-	postfix[j]='\0';
-	printf("\n Postfix Expression Converion is: %s",postfix);
-
-getch();
-}
-int precedence(char elem)
-{
-	switch(elem)
-	{
-	case '+':
-	case '-': return(1);
-	case '*':
-	case '/': return(2);
-	case '^': return(3);
-	case '(':
-	case '#': return(0);
-	}
-	return 0;
+void push(char elem) {
+    if (top >= MAX - 1) {
+        printf("\nStack Overflow!");
+    } else {
+        stack[++top] = elem;
+    }
 }
 
-void push(char elem)
-{
-	++top;
-	s[top]=elem;
+char pop() {
+    if (top == -1) {
+        printf("\nStack Underflow!");
+        return '\0';
+    } else {
+        return stack[top--];
+    }
 }
 
-int pop()
-{
-	char elem;
-	elem=s[top];
-	--top;
-	return(elem);
+int precedence(char op) {
+    switch (op) {
+        case '+':
+        case '-': return 1;
+        case '*':
+        case '/': return 2;
+        case '^': return 3;
+        case '(': return 0;  // Lowest precedence for '('
+        default : return -1;
+    }
+}
+
+void infixToPostfix(char infix[], char postfix[]) {
+    int i = 0, j = 0;
+    char ch, elem;
+
+    push('#');  // Sentinel
+
+    while ((ch = infix[i++]) != '\0') {
+        if (isalnum(ch)) {
+            postfix[j++] = ch;
+        } else if (ch == '(') {
+            push(ch);
+        } else if (ch == ')') {
+            while (stack[top] != '(') {
+                postfix[j++] = pop();
+            }
+            pop();  // Pop the '('
+        } else {
+            while (precedence(stack[top]) >= precedence(ch)) {
+                postfix[j++] = pop();
+            }
+            push(ch);
+        }
+    }
+
+    while (stack[top] != '#') {
+        postfix[j++] = pop();
+    }
+
+    postfix[j] = '\0';  // Null terminate the postfix expression
+}
+
+int main() {
+    char infix[MAX], postfix[MAX];
+
+    printf("\nInfix to Postfix Conversion");
+    printf("\n---------------------------");
+    printf("\nEnter an infix expression: ");
+    scanf("%s", infix);
+
+    infixToPostfix(infix, postfix);
+
+    printf("Postfix Expression: %s\n", postfix);
+
+    return 0;
 }
