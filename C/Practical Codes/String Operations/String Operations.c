bd05ac41373fa9e68f3dbfa2669898e418c04606
John-Valinsky <johnvalinsky07@gmail.com>
2025-04-15 02:21:43 +0530

    update

--- C/Practical Codes/String Operations/String Operations.c
+++ C/Practical Codes/String Operations/String Operations.c
@@ -1,167 +1,135 @@
-// 9. Given S1={"Flowers"} ; S2={"are beautiful"},
-// a) Find the length of Si.
-// b) Concatenate Si and S2. 
-// c) Extract the substring "low" from Si. 
-// d) Find "are" in S2 and replace it with "is".
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#define MAX_LEN 100
 
-int LENGTH(char *str) {
-    int i=0;
-    while(str[i] !='\0')
-	i++;
+// Function Prototypes
+int LENGTH(const char *str);
+void CONCAT(char *str1, const char *str2);
+void SUBSTR(const char *str, int pos, int len);
+void REPLACE(const char *str, const char *substr, const char *replacestr);
+
+int LENGTH(const char *str) {
+    int i = 0;
+    while (str[i] != '\0') i++;
     return i;
 }
 
-void CONCAT(char *str1, char *str2) {
-    int i=0, j=0;
-    while(str1[i]!='\0')
-	i++;
-    while(str2[j]!='\0') {
-	str1[i]=str2[j];
-	i++;
-	j++;
-    }
-    // Append '\0' to the end of the first string
-    str1[i]='\0';
+void CONCAT(char *str1, const char *str2) {
+    strcat(str1, str2);
+    printf("\nConcatenated String: %s\n", str1);
 }
 
-void SUBSTR(char str[], int pos, int len)
-{
+void SUBSTR(const char *str, int pos, int len) {
+    int strLen = strlen(str);
+    if (pos < 1 || pos > strLen || len < 1 || (pos - 1 + len) > strLen) {
+        printf("\nInvalid Position or Length for Substring.\n");
+        return;
+    }
+
     char sub[50];
-    int slen = strlen(str);
-    int j, max_ext;
-
-    if(pos <= 0 || pos > slen || len <= 0)
-    {
-	printf("\n Invalid Position or Substring Length ");
-	return;
-    }
-
-    max_ext = slen - pos + 1;
-    if(len > max_ext)
-    {
-	printf("\n Invalid Substring Length ");
-	return;
-    }
-
-    for(j = 0; j < len; j++)
-    {
-	sub[j] = str[pos - 1 + j];
-    }
-    sub[j] = '\0';
-
-    printf("\n Substring = %s", sub);
+    strncpy(sub, str + pos - 1, len);
+    sub[len] = '\0';
+    printf("\nSubstring: %s\n", sub);
 }
 
-void REPLACE(char str[], char substr[], char replacestr[])
-{
-    char output[50];
-    int i=0, j=0, flag=0, start=0;
+void REPLACE(const char *str, const char *substr, const char *replacestr) {
+    char result[MAX_LEN] = "";
+    const char *pos = strstr(str, substr);
 
-    // To check whether substring present in a string or not
-    while(str[i] != '\0')
-    {
-	if (str[i] == substr[j])
-	{
-	    if(!flag)
-		start = i;
-	    j++;
-	    if(substr[j] == '\0')
-		break;
-	    flag = 1;
-	}
-	else
-	{
-	    flag = start = j = 0;
-	}
-	i++;
+    if (pos == NULL) {
+        printf("\n'%s' is not found in '%s'\n", substr, str);
+        return;
     }
-    if(substr[j] == '\0' && flag)
-    {
-	for(i=0; i<start; i++)
-	    output[i] = str[i];
-	// Replace substring with another string
-	for(j=0; j<strlen(replacestr); j++)
-	{
-	    output[i] = replacestr[j];
-	    i++;
-	}
-	// Copy remaining portion of the input string "str"
-	for(j = start + strlen(substr); j< strlen(str); j++)
-	{
-	    output[i] = str[j];
-	    i++;
-	}
-	// Print the final string
-	output[i] = '\0';
-	printf("\n Output: %s\n", output);
-    }
-    else
-	printf("\n %s is not a substring of %s\n", substr, str);
+
+    // Copy characters before substring
+    strncpy(result, str, pos - str);
+    result[pos - str] = '\0';
+
+    // Concatenate replacement string
+    strcat(result, replacestr);
+
+    // Concatenate remaining string after substring
+    strcat(result, pos + strlen(substr));
+
+    printf("\nModified String: %s\n", result);
 }
 
-int main()
-{
-    char str1[50], str2[50], substr[50], repstr[50];
-    int len, pos, ch;
-    clrscr();
+int main() {
+    char str1[MAX_LEN], str2[MAX_LEN], substr[50], replacestr[50];
+    int choice, pos, len;
 
-    while(1)
-    {
-	printf("\n****************************");
-	printf("\n 1.String Operations");
-	printf("\n 2.String Concatenation");
-	printf("\n 3.Extracting Substring");
-	printf("\n 4.Replace a String");
-	printf("\n 5.Exit");
-	printf("\n****************************\n");
-	printf("\n Enter your choice:  ");
-	scanf("%d",&ch);
-	fflush(stdin);
-	clrscr();
+    while (1) {
+        printf("\n========== STRING OPERATIONS ==========\n");
+        printf("1. Find Length of a String\n");
+        printf("2. Concatenate Two Strings\n");
+        printf("3. Extract a Substring\n");
+        printf("4. Replace a Word in a String\n");
+        printf("5. Exit\n");
+        printf("=======================================\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        getchar(); // Consume newline from previous input
 
-	switch(ch)
-	{
-	    case 1: printf("\n Enter the String: ");
-		    scanf("%s",&str1);
-		    printf("\n The length of a String: %d", LENGTH(str1));
-		    break;
+        switch (choice) {
+            case 1:
+                printf("\nEnter the string: ");
+                fgets(str1, MAX_LEN, stdin);
+                str1[strcspn(str1, "\n")] = '\0'; // Remove newline
+                printf("Length of string: %d\n", LENGTH(str1));
+                break;
 
-	    case 2: printf("\n Enter the First String: ");
-		    scanf("%s",&str1);
-		    printf("\n Enter the Second String: ");
-		    scanf("%s",&str2);
-		    CONCAT(str1, str2);
-		    printf("\n Concatenated String: %s",str1);
-		    break;
+            case 2:
+                printf("\nEnter the first string: ");
+                fgets(str1, MAX_LEN, stdin);
+                str1[strcspn(str1, "\n")] = '\0';
 
-	    case 3: printf("\n Enter the String: ");
-		    scanf("%s",&str1);
-		    printf("\n Enter the Position of a Substring: ");
-		    scanf("%d",&pos);
-		    printf("\n Enter the Length of a Substring: ");
-		    scanf("%d",&len);
-		    SUBSTR(str1,pos,len);
-		    break;
+                printf("Enter the second string: ");
+                fgets(str2, MAX_LEN, stdin);
+                str2[strcspn(str2, "\n")] = '\0';
 
-	    case 4: printf("\n Enter the String: ");
-		    scanf("%s",&str1);
-		    printf("\n Enter the string to be removed: ");
-		    scanf("%s",&substr);
-		    printf("\n Enter the string to replace: ");
-		    scanf("%s",&repstr);
-		    REPLACE(str1, substr, repstr);
-		    break;
+                CONCAT(str1, str2);
+                break;
 
-	    case 5: exit(0);
+            case 3:
+                printf("\nEnter the string: ");
+                fgets(str1, MAX_LEN, stdin);
+                str1[strcspn(str1, "\n")] = '\0';
 
-	    default: printf("\n Invalid Option");
-		     break;
-	}
+                printf("Enter the starting position: ");
+                scanf("%d", &pos);
+                printf("Enter the length of substring: ");
+                scanf("%d", &len);
+                getchar();
 
-	getch();
+                SUBSTR(str1, pos, len);
+                break;
+
+            case 4:
+                printf("\nEnter the original string: ");
+                fgets(str1, MAX_LEN, stdin);
+                str1[strcspn(str1, "\n")] = '\0';
+
+                printf("Enter the word to replace: ");
+                fgets(substr, sizeof(substr), stdin);
+                substr[strcspn(substr, "\n")] = '\0';
+
+                printf("Enter the new word: ");
+                fgets(replacestr, sizeof(replacestr), stdin);
+                replacestr[strcspn(replacestr, "\n")] = '\0';
+
+                REPLACE(str1, substr, replacestr);
+                break;
+
+            case 5:
+                printf("\nExiting program. Goodbye!\n");
+                exit(0);
+
+            default:
+                printf("\nInvalid choice. Please try again.\n");
+        }
     }
+
+    return 0;
 }
