986644b7e26186db25d3136498e0ec2ef2072ccb
John Lalremruata <johnvalinsky07@gmail.com>
2025-05-04 01:48:53 +0530

    update

--- C++/Nested if else/Codes.cpp
+++ C++/Nested if else/Codes.cpp
@@ -9,3 +9,22 @@ int main() {
 
     // Input three numbers
     cout << "Enter three numbers: ";
+    cin >> num1 >> num2 >> num3;
+
+    // Nested if...else to find the greatest number
+    if (num1 >= num2) {
+        if (num1 >= num3) {
+            cout << "The greatest number is: " << num1 << endl;
+        } else {
+            cout << "The greatest number is: " << num3 << endl;
+        }
+    } else {
+        if (num2 >= num3) {
+            cout << "The greatest number is: " << num2 << endl;
+        } else {
+            cout << "The greatest number is: " << num3 << endl;
+        }
+    }
+
+    return 0;
+}
