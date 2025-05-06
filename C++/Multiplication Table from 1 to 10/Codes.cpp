6f5f1287d2c0f3c28825c40073bc027f46d7c474
John Lalremruata <johnvalinsky07@gmail.com>
2025-05-07 00:31:17 +0530

    update

--- C++/Multiplication Table from 1 to 10/Codes.cpp
+++ C++/Multiplication Table from 1 to 10/Codes.cpp
@@ -14,3 +14,4 @@ int main() {
         for (int j = 1; j <= 10; ++j) {
             cout << i << " x " << j << " = " << i * j << endl;
         }
+        cout << "--------------------------" << endl;
