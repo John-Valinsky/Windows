cd06ac70e61f0309ab164ec9b6d4c2a0194075f4
John Lalremruata <johnvalinsky07@gmail.com>
2025-05-07 00:37:00 +0530

    update

--- C++/Two numbers using pass by value/Codes.cpp
+++ C++/Two numbers using pass by value/Codes.cpp
@@ -6,3 +6,8 @@ using namespace std;
 // Function to swap numbers (pass by value)
 void swapNumbers(int a, int b) {
     int temp = a;
+    b = temp;
+
+    cout << "Inside swap function:" << endl;
+}
+
