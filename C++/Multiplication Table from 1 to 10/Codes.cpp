294a8551b827ed3ecc046281825679aafdc63b0a
John Lalremruata <johnvalinsky07@gmail.com>
2025-05-07 00:30:31 +0530

    update

--- C++/Multiplication Table from 1 to 10/Codes.cpp
+++ C++/Multiplication Table from 1 to 10/Codes.cpp
@@ -6,5 +6,9 @@ using namespace std;
 int main() {
 
     // Outer loop for numbers 1 to 10
-    for (int i = 1; i <= 10; ++i) {
+    	for (int i = 1; i <= 10; ++i) {
         cout << "Multiplication Table of " << i << ":\n";
+
+
+   // Inner loop to multiply by 1 to 10
+        for (int j = 1; j <= 10; ++j) {
