e13e50dda7ba4fb3ae268ed729580434916919f4
John Lalremruata <johnvalinsky07@gmail.com>
2025-05-08 02:20:55 +0530

    update

--- C++/Swapping of two numbers/Codes.cpp
+++ C++/Swapping of two numbers/Codes.cpp
@@ -1,3 +1,8 @@
 //Q. Write a Program for Swapping of two numbers using pass by address.
 
 #include <iostream>
+using namespace std;
+
+
+// Function to swap numbers using pointers (pass by address)
+void swapNumbers(int *a, int *b) {
