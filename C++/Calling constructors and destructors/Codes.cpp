606217cfd4756f177f1677c474ee47d2e1adaef1
John Lalremruata <johnvalinsky07@gmail.com>
2025-05-16 01:12:49 +0530

    update

--- C++/Calling constructors and destructors/Codes.cpp
+++ C++/Calling constructors and destructors/Codes.cpp
@@ -1,2 +1,33 @@
 //Q. Write a program to show the ways of calling constructors and destructors.
 
+#include <iostream>
+using namespace std;
+
+class Demo {
+private:
+    int value;
+
+public:
+    // Default constructor
+    Demo() {
+        value = 0;
+        cout << "Default constructor called. Value = " << value << endl;
+    }
+
+    // Parameterized constructor
+    Demo(int val) {
+        value = val;
+        cout << "Parameterized constructor called. Value = " << value << endl;
+    }
+
+    // Copy constructor
+    Demo(const Demo &obj) {
+        value = obj.value;
+        cout << "Copy constructor called. Copied Value = " << value << endl;
+    }
+
+    // Destructor
+    ~Demo() {
+        cout << "Destructor called for Value = " << value << endl;
+    }
+};
