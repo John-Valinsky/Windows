32dbc5974d6a056b4e6081b3f226a031e5e73ae5
John Lalremruata <johnvalinsky07@gmail.com>
2025-05-21 00:13:53 +0530

    update

---
+++ C++/Create some objects/Codes.cpp
@@ -0,0 +1,6 @@
+//Q. Create the Person class. Create some objects of this class (by taking information from the user).
+//Inherit the class Person to create two classes Teacher and Student class.
+//Maintain the respective information in the classes and create, display and delete
+//objects of these two classes (Use Runtime Polymorphism).
+
+
