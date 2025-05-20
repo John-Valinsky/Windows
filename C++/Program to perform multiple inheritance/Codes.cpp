2d7f1b8ab7fb28360a771af58de55412f2f590a4
John Lalremruata <johnvalinsky07@gmail.com>
2025-05-21 00:10:50 +0530

    update

--- C++/Program to perform multiple inheritance/Codes.cpp
+++ C++/Program to perform multiple inheritance/Codes.cpp
@@ -1,3 +1,56 @@
 //Q. Write a program to perform multiple inheritance.
 
+#include <iostream>
+using namespace std;
 
+// First base class
+class Teacher {
+public:
+    string subject;
+
+    void getSubject() {
+        cout << "Enter subject taught: ";
+        cin >> subject;
+    }
+
+    void showSubject() {
+        cout << "Subject: " << subject << endl;
+    }
+};
+
+// Second base class
+class Staff {
+public:
+    int staffID;
+
+    void getStaffID() {
+        cout << "Enter staff ID: ";
+        cin >> staffID;
+    }
+
+    void showStaffID() {
+        cout << "Staff ID: " << staffID << endl;
+    }
+};
+
+// Derived class from Teacher and Staff
+class Faculty : public Teacher, public Staff {
+public:
+    void showFacultyDetails() {
+        showSubject();
+        showStaffID();
+    }
+};
+
+int main() {
+    Faculty f;
+
+    cout << "--- Enter Faculty Details ---" << endl;
+    f.getSubject();
+    f.getStaffID();
+
+    cout << "\n--- Faculty Details ---" << endl;
+    f.showFacultyDetails();
+
+    return 0;
+}
