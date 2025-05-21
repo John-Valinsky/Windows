d10000cec7ac942cc88c6026f429ffba804eb8bc
John Lalremruata <johnvalinsky07@gmail.com>
2025-05-22 00:33:55 +0530

    update

--- C++/Vol. II/Array implementation of Stack/Code.cpp
+++ C++/Vol. II/Array implementation of Stack/Code.cpp
@@ -1,3 +1,12 @@
 //Q. Array implementation of Stack.
 
+#include <iostream>
+using namespace std;
+
+#define MAX 100  // Maximum size of the stack
+
+class Stack {
+    int arr[MAX];  // Array to hold stack elements
+    int top;       // Index of the top element in the stack
+
 
