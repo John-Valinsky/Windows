d63a11fac721672cc73eb60aedd2d78c7a35e76d
John Lalremruata <johnvalinsky07@gmail.com>
2025-05-22 00:36:38 +0530

    update

--- C++/Vol. II/Array implementation of Linear Queue/Code.cpp
+++ C++/Vol. II/Array implementation of Linear Queue/Code.cpp
@@ -1,3 +1,12 @@
 //Q. Array implementation of Linear Queue.
 
+#include <iostream>
+using namespace std;
+
+#define MAX 5  // Maximum size of the queue
+
+class Queue {
+    int arr[MAX];  // Array to hold queue elements
+    int front, rear;  // Indices for the front and rear of the queue
+
 
