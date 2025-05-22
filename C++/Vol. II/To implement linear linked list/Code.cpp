4c9e5b1315a431517c49aac87c74e7a92965743a
John Lalremruata <johnvalinsky07@gmail.com>
2025-05-23 01:18:03 +0530

    update

--- C++/Vol. II/To implement linear linked list/Code.cpp
+++ C++/Vol. II/To implement linear linked list/Code.cpp
@@ -1,3 +1,16 @@
 //Q. To implement linear linked list and perform different operation such as node insert and delete, search of an item, reverse the list.
 
+#include <iostream>
+using namespace std;
+
+// Definition of the node structure
+struct Node {
+    int data;       // Data to store
+    Node* next;     // Pointer to the next node
+
+    Node(int value) {
+        data = value;
+        next = nullptr;
+    }
+};
 
