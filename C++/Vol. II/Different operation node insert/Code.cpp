61d63c0332e0c1ff6b7e67b806fa266620949b49
John Lalremruata <johnvalinsky07@gmail.com>
2025-05-24 12:18:38 +0530

    update

--- C++/Vol. II/Different operation node insert/Code.cpp
+++ C++/Vol. II/Different operation node insert/Code.cpp
@@ -1,2 +1,17 @@
 //Q. To implement double linked list and perform different operation such as node insert and delete.
 
+#include <iostream>
+using namespace std;
+
+// Definition of the node structure
+struct Node {
+    int data;       // Data to store
+    Node* next;     // Pointer to the next node
+    Node* prev;     // Pointer to the previous node
+
+    Node(int value) {
+        data = value;
+        next = nullptr;
+        prev = nullptr;
+    }
+};
