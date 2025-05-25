7af9526dbb7fba889e7e36d0d978d3d13aa646fe
John Lalremruata <johnvalinsky07@gmail.com>
2025-05-25 12:00:01 +0530

    update

--- C++/Vol. II/Linked list implementation of Queue/Code.cpp
+++ C++/Vol. II/Linked list implementation of Queue/Code.cpp
@@ -14,4 +14,29 @@ struct Node {
     }
 };
 
+class Queue {
+private:
+    Node* front; // Pointer to the front of the queue
+    Node* rear;  // Pointer to the rear of the queue
+
+public:
+    // Constructor to initialize the queue
+    Queue() {
+        front = rear = nullptr; // Initially the queue is empty
+    }
+
+    // Function to enqueue an element (add to the rear)
+    void enqueue(int value) {
+        Node* newNode = new Node(value);
+
+        // If the queue is empty, both front and rear will point to the new node
+        if (rear == nullptr) {
+            front = rear = newNode;
+        } else {
+            rear->next = newNode;  // Add new node at the end of the queue
+            rear = newNode;        // Update rear to the new node
+        }
+        cout << value << " enqueued to the queue." << endl;
+    }
+
 
