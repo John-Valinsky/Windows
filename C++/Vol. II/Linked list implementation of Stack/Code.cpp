24ad3b1c54cb517f546ae92b9ae6b9ccc574a907
John Lalremruata <johnvalinsky07@gmail.com>
2025-05-25 11:55:52 +0530

    update

--- C++/Vol. II/Linked list implementation of Stack/Code.cpp
+++ C++/Vol. II/Linked list implementation of Stack/Code.cpp
@@ -1,3 +1,16 @@
 //Q. Linked list implementation of Stack.
 
+#include <iostream>
+using namespace std;
+
+// Definition of the Node structure
+struct Node {
+    int data;   // Data to store
+    Node* next; // Pointer to the next node
+
+    Node(int value) {
+        data = value;
+        next = nullptr;
+    }
+};
 
