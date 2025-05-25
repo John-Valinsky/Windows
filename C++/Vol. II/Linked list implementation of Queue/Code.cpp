b819895704ae845bb4884b3e037c6775b1c182ae
John Lalremruata <johnvalinsky07@gmail.com>
2025-05-25 11:59:44 +0530

    update

--- C++/Vol. II/Linked list implementation of Queue/Code.cpp
+++ C++/Vol. II/Linked list implementation of Queue/Code.cpp
@@ -1,3 +1,17 @@
 //Q. Linked list implementation of Queue.
 
+#include <iostream>
+using namespace std;
+
+// Definition of the Node structure
+struct Node {
+    int data;   // Data to store
+    Node* next; // Pointer to the next node
+
+    Node(int value) {
+        data = value;
+        next = nullptr;
+    }
+};
+
 
