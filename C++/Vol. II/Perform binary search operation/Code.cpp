f71deefce56188f554f146e94202ed5ddf22a558
John Lalremruata <johnvalinsky07@gmail.com>
2025-05-27 00:41:41 +0530

    update

--- C++/Vol. II/Perform binary search operation/Code.cpp
+++ C++/Vol. II/Perform binary search operation/Code.cpp
@@ -1,3 +1,10 @@
 //Q. To perform binary search operation.
 
-}
+#include <iostream>
+using namespace std;
+
+// Function to perform binary search
+int binarySearch(int arr[], int size, int target) {
+    int low = 0;
+    int high = size - 1;
+
