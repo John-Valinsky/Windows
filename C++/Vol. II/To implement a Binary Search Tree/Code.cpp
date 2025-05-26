8e76ecfc6a78aeb8b690bf14dd2d1989253d5178
John Lalremruata <johnvalinsky07@gmail.com>
2025-05-26 10:58:14 +0530

    update

--- C++/Vol. II/To implement a Binary Search Tree/Code.cpp
+++ C++/Vol. II/To implement a Binary Search Tree/Code.cpp
@@ -1,3 +1,19 @@
 //Q. To implement a Binary Search Tree.
 
+#include <iostream>
+using namespace std;
+
+// Definition of the structure of a node in BST
+struct Node {
+    int data;       // Data to store in the node
+    Node* left;     // Pointer to the left child
+    Node* right;    // Pointer to the right child
+
+    // Constructor to initialize the node
+    Node(int value) {
+        data = value;
+        left = nullptr;
+        right = nullptr;
+    }
+};
 
