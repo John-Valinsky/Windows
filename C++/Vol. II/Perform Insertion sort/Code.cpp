176bb59f36d54680af4e668e2db3b2ee5e7fa854
John Lalremruata <johnvalinsky07@gmail.com>
2025-05-28 10:28:04 +0530

    update

--- C++/Vol. II/Perform Insertion sort/Code.cpp
+++ C++/Vol. II/Perform Insertion sort/Code.cpp
@@ -1,3 +1,23 @@
 //Q. To perform Insertion sort.
 
+#include <iostream>
+using namespace std;
+
+// Function to perform Insertion Sort
+void insertionSort(int arr[], int size) {
+    for (int i = 1; i < size; i++) {
+        int key = arr[i];  // Element to be inserted
+        int j = i - 1;
+
+        // Shift elements that are greater than key to one position ahead
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];  // Shift the element
+            j--;
+        }
+
+        // Insert the key at its correct position
+        arr[j + 1] = key;
+    }
+}
+
 
