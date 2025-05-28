d5dda7d58eb17a1618426df6b52328fb0988546b
John Lalremruata <johnvalinsky07@gmail.com>
2025-05-29 00:20:51 +0530

    update

--- C++/Vol. II/Perform Quick sort/Code.cpp
+++ C++/Vol. II/Perform Quick sort/Code.cpp
@@ -3,3 +3,7 @@
 #include <iostream>
 using namespace std;
 
+// Function to perform partition
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];  // Choose the last element as pivot
+    int i = low - 1;  // Index of smaller element
