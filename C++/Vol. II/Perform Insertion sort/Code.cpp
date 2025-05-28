de84238933ee60934e26856aade80e5b6b29b4a0
John Lalremruata <johnvalinsky07@gmail.com>
2025-05-28 10:28:17 +0530

    update

--- C++/Vol. II/Perform Insertion sort/Code.cpp
+++ C++/Vol. II/Perform Insertion sort/Code.cpp
@@ -20,4 +20,25 @@ void insertionSort(int arr[], int size) {
     }
 }
 
+// Function to print the array
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
 
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Original array: ";
+    printArray(arr, size);
+
+    insertionSort(arr, size);  // Sort the array
+
+    cout << "Sorted array: ";
+    printArray(arr, size);
+
+    return 0;
+}
