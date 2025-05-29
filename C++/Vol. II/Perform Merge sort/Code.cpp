d0b78861ef9666ae127ef5c05e8ae9caaf3bfc09
John Lalremruata <johnvalinsky07@gmail.com>
2025-05-30 00:15:41 +0530

    update

--- C++/Vol. II/Perform Merge sort/Code.cpp
+++ C++/Vol. II/Perform Merge sort/Code.cpp
@@ -16,3 +16,7 @@ void merge(int arr[], int l, int m, int r) {
 
     // Copy data to temporary arrays L[] and R[]
     for (int i = 0; i < n1; i++)
+
+     L[i] = arr[l + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
