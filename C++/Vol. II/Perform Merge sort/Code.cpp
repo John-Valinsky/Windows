31fd8e408f64037ff222c835228af329dcfbb5dd
John Lalremruata <johnvalinsky07@gmail.com>
2025-05-30 00:15:23 +0530

    update

--- C++/Vol. II/Perform Merge sort/Code.cpp
+++ C++/Vol. II/Perform Merge sort/Code.cpp
@@ -10,3 +10,9 @@ void merge(int arr[], int l, int m, int r) {
     // Find sizes of two subarrays to be merged
     int n1 = m - l + 1;
     int n2 = r - m;
+
+      // Create temporary arrays
+    int L[n1], R[n2];
+
+    // Copy data to temporary arrays L[] and R[]
+    for (int i = 0; i < n1; i++)
